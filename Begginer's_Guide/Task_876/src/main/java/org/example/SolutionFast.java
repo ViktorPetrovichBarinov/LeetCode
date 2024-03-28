83d78be4fd06bad09519c058c758d3f876bd809d
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-03-29 00:00:15 +0700

    876 меньше константа

---
+++ Begginer's_Guide/Task_876/src/main/java/org/example/SolutionFast.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+public class SolutionFast {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
