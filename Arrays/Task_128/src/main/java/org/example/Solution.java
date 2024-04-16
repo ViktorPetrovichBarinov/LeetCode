6f7f13a96ad4a55b5b833e06977d20aa816f7439
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-04-16 15:41:21 +0700

    128 nlogn

--- Arrays/Task_128/src/main/java/org/example/Solution.java
+++ Arrays/Task_128/src/main/java/org/example/Solution.java
@@ -1,47 +1,46 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int num : nums) {
-            if (num < min) {
-                min = num;
-            }
-            if (num > max) {
-                max = num;
-            }
+        if (nums.length == 0 || nums.length == 1) {
+            return nums.length;
         }
-
-        int histLength = max - min + 1;
-        boolean[] histogram = new boolean[histLength];
-
+        HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
-            histogram[num - min] = true;
+            set.add(num);
         }
-
-        int answer = 0;
-        int currentLength = 0;
-        for (boolean b : histogram) {
-            if (currentLength != 0) {
-                if (b) {
+        int[] numsWithoutDuplicates = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            numsWithoutDuplicates[index++] = num;
+        }
+        Arrays.sort(numsWithoutDuplicates);
+        int lastElem = numsWithoutDuplicates[0];
+        int maxLength = 1;
+        int currentLength = 1;
+        for (int i = 1; i < numsWithoutDuplicates.length; i++) {
+            if (currentLength == 1) {
+                if (numsWithoutDuplicates[i] - 1 == lastElem) {
                     currentLength++;
-                } else {
-                    answer = Math.max(currentLength, answer);
-                    currentLength = 0;
                 }
             } else {
-                if (b) {
+                if (numsWithoutDuplicates[i] - 1 == lastElem) {
                     currentLength++;
+                } else {
+                    maxLength = Math.max(maxLength, currentLength);
+                    currentLength = 1;
                 }
             }
+            lastElem = numsWithoutDuplicates[i];
         }
-        return Math.max(answer, currentLength);
+        return Math.max(maxLength, currentLength);
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.longestConsecutive(new int[]{100,4,200,1,3,2}));
+        System.out.println(sol.longestConsecutive(new int[]{1, 2, 0, 1}));
     }
 }
