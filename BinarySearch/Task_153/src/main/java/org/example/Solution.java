f875efe442c2359d8dfb74349218f67779c187bc
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-11 03:30:09 +0700

    153 с бин поиском

--- BinarySearch/Task_153/src/main/java/org/example/Solution.java
+++ BinarySearch/Task_153/src/main/java/org/example/Solution.java
@@ -3,13 +3,22 @@ package org.example;
 
 public class Solution {
     public int findMin(int[] nums) {
-        int min = 10000;
-        for (int i = 0; i < nums.length; i++) {
-            min = Math.min(min, nums[i]);
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= nums[left] && nums[left] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                return nums[mid];
+            }
         }
-        return min;
+        return -1;
     }
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        System.out.println(sol.findMin(new int[]{2, 1}));
     }
 }
