7d88743b9fb81f829a6b132958315a15412154ea
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-11 03:32:35 +0700

    Update Solution.java

--- BinarySearch/Task_153/src/main/java/org/example/Solution.java
+++ BinarySearch/Task_153/src/main/java/org/example/Solution.java
@@ -5,17 +5,15 @@ public class Solution {
     public int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        while(left <= right) {
+        while(left < right) {
             int mid = (left + right) / 2;
-            if (nums[mid] >= nums[left] && nums[left] > nums[right]) {
-                left = mid + 1;
-            } else if (nums[mid] < nums[right]) {
+            if (nums[mid] < nums[right]) {
                 right = mid;
             } else {
-                return nums[mid];
+                left = mid + 1;
             }
         }
-        return -1;
+        return nums[left];
     }
     public static void main(String[] args) {
         Solution sol = new Solution();
