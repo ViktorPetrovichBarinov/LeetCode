15952598be28fa5109952b94e15dba8fd2b7f535
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-26 04:19:23 +0700

    1493

--- Task_1493/src/main/java/org/example/Solution.java
+++ Task_1493/src/main/java/org/example/Solution.java
@@ -2,50 +2,46 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
     public int longestSubarray(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int max = 0;
-        int length = nums.length;
-        int currentLength = 0;
-        int currentNulls = 0;
-        for (int i = 0; i < length; i++) {
-            int currentElem = nums[i];
-            if (currentElem == 1) {
-                currentLength++;
-                currentNulls = 0;
-            } else {
-                if (currentNulls == 0) {
-                    currentNulls++;
-                    list.add(currentLength);
-                    currentLength = 0;
-                } else {
-                    if (list.size() == 1) {
-                        max = Math.max(max, list.get(0) - 1);
-                    } else {
-                        int currentSum = list.get(0);
-                        for (int j = 1; j < list.size(); j++) {
-                            currentSum += list.get(j);
-                            max = Math.max(max, currentSum);
-                            currentSum -= list.get(j-1);
-                        }
-                        list = new ArrayList<>();
-                        currentNulls = 0;
-                    }
-                }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                break;
+            }
+            if (i == nums.length - 1) {
+                return 0;
             }
         }
-        list.add(currentLength);
-        if (list.size() == 1) {
-            max = Math.max(max, list.get(0) - 1);
-        } else {
-            int currentSum = list.get(0);
-            for (int j = 1; j < list.size(); j++) {
-                currentSum += list.get(j);
-                max = Math.max(max, currentSum);
-                currentSum -= list.get(j-1);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1) {
+                break;
             }
+            if (i == nums.length - 1) {
+                return nums.length - 1;
+            }
+        }
+
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+        int prev = 0;
+        while(index < nums.length) {
+            while(index < nums.length && nums[index] != 1) {
+                index++;
+            }
+            int current = 0;
+            while(index < nums.length && nums[index] != 0) {
+                index++;
+                current++;
+            }
+            max = Math.max(max, current + prev);
+            prev = current;
+            if (index + 1 < nums.length && nums[index + 1] == 0) {
+                prev = 0;
+            }
+
         }
         return max;
     }
@@ -54,7 +50,7 @@ public class Solution {
         int[] arr1 = new int[]{1,1,1};
         int[] arr2 = new int[]{1,1,1,0,1,1,0,1,0,0,0};
 
-        System.out.println(sol.longestSubarray(arr1));
+        //System.out.println(sol.longestSubarray(arr1));
         System.out.println(sol.longestSubarray(arr2));
     }
 }
